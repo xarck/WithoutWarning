a899416fcf4035a9508b9abab615957b62c012a3
Ashish <iamashking123@gmail.com>
2024-04-16 19:53:59 +0530

    done

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 import urllib.request
 import json
 import os
-from dotenv import load_dotenv
+
 import ssl
 load_dotenv()
 
@@ -39,9 +39,9 @@ This is the message of the user ${input_text}.
 
     body = str.encode(json.dumps(data))
 
-    url = st.secrets["URL"]
+    url = 'https://knacktocode-pmqaa.eastus2.inference.ml.azure.com/score'
     # Replace this with the primary/secondary key or AMLToken for the endpoint
-    api_key = st.secrets["KEY"]
+    api_key = '4knTxH4pDTjlu6ZqxAdT0hxMDGROyKtQ'
     if not api_key:
         raise Exception("A key should be provided to invoke the endpoint")
 
